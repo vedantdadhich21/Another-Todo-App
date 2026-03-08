e1af77fccb11e7c46e955aeb6a7fd0ff6f1ae2c6
Vedant Dadhich <vedantdadhich21@gmail.com>
2026-03-08 17:49:13 +0530

    responsive design

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import "./todo.js"
 import { renderNavbar } from "./ui/renderNavbar.js"
 import { renderTodos } from "./ui/renderTodo.js"
 
+document.getElementById("menuToggle")
+  .addEventListener("click", () => {
+    document.querySelector("nav").classList.toggle("open");
+  });
+  
 renderNavbar()
 renderTodos()
